3dd71db2691938554122143827eb59250d63de3d
Mansoor-P <munaf6007@gmail.com>
2024-07-15 14:22:38 +0530

    company repository extends JpaRepository

---
+++ src/main/java/com/mansoor/jobapp/company/CompanyRepository.java
@@ -0,0 +1,8 @@
+package com.mansoor.jobapp.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company,Long> {
+}
