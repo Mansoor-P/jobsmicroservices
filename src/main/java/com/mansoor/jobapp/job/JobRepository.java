4f0a0fcfe3322069f4a2a472af1bdb22dfe3e4e3
Mansoor-P <munaf6007@gmail.com>
2024-07-15 14:19:34 +0530

    job repository extends JpaRepository

---
+++ src/main/java/com/mansoor/jobapp/job/JobRepository.java
@@ -0,0 +1,8 @@
+package com.mansoor.jobapp.job;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job,Long> {
+}
