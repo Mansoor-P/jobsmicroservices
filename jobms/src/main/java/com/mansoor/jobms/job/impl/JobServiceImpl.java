0a8bd3d9640c62654ea5bb2bb0959641eac09f84
Mansoor-P <munaf6007@gmail.com>
2024-08-14 12:55:19 +0530

    feat: add fallback implementation for resilient service communication

--- jobms/src/main/java/com/mansoor/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/mansoor/jobms/job/impl/JobServiceImpl.java
@@ -41,15 +41,23 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    @CircuitBreaker(name = "companyBreaker")
+    @CircuitBreaker(name = "companyBreaker",
+            fallbackMethod = "companyBreakerFallback")
     public List<JobDTO> findAll() {
 
         List<Job> jobs = jobRepository.findAll();
         List<JobDTO> jobDTOS = new ArrayList<>();
 
-
         return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
     }
+
+    public List<String> companyBreakerFallback(Exception e) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Dummy Error");
+        return list;
+    }
+
 
     private JobDTO convertToDto(Job job) {
         Company company = companyClient.getCompany(job.getCompanyId());
